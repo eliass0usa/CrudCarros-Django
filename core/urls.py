d6ac15f2d5560ba82c6a12d432754e996caec1ca
eliassousa <eliassousaoliveira@hotmail.com>
2024-08-25 23:43:36 -0300

    feat: default homepage

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, salvar, editar, update, delete
+from .views import salvar, editar, update, delete
 
 urlpatterns = [
-    path('', home),
     path('salvar/', salvar, name="salvar"),
     path('editar/<int:id>', editar, name="editar"),
     path('update/<int:id>', update, name="update"),
