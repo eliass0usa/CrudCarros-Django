985c97c562887ce8a983f40698550090e6152497
eliassousa <eliassousaoliveira@hotmail.com>
2024-09-07 21:41:13 -0300

    feat: user register in admin console

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Cliente, Carro
+from .models import Cliente, Carro, User
 
 admin.site.register(Carro)
 admin.site.register(Cliente)
+admin.site.register(User)
